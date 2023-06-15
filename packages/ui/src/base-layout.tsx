54677411a2f0af8eb81c2ceb6b03de9de1090555
Mariusz Winnik <Mariusz.Winnik@amrest.eu>
2023-06-15 12:06:38 +0200

    Center layout

--- packages/ui/src/base-layout.tsx
+++ packages/ui/src/base-layout.tsx
@@ -1,6 +1,6 @@
 export function BaseLayout({ children }) {
   return (
-    <div className="flex flex-col min-h-screen px-28 pt-6 pb-20">
+    <div className="flex flex-col min-h-screen items-center px-28 pt-6 pb-20">
       {children}
     </div>
   );
